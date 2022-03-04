7efdad2847a1b6de19c5b6c23fe445f141825f92
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-04 08:58:30 -0500

    Added component to hold all Expense Items

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const ExpenseItems = [
@@ -10,7 +10,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -27,26 +32,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={ExpenseItems[0].title}
-        amount={ExpenseItems[0].amount}
-        date={ExpenseItems[0].date}
-      />
-      <ExpenseItem
-        title={ExpenseItems[1].title}
-        amount={ExpenseItems[1].amount}
-        date={ExpenseItems[1].date}
-      />
-      <ExpenseItem
-        title={ExpenseItems[2].title}
-        amount={ExpenseItems[2].amount}
-        date={ExpenseItems[2].date}
-      />
-      <ExpenseItem
-        title={ExpenseItems[3].title}
-        amount={ExpenseItems[3].amount}
-        date={ExpenseItems[3].date}
-      />
+      <Expenses items={ExpenseItems} />
     </div>
   );
 }
