b3d36282b51b8fc274d6d2ebb7d9659d7c095242
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-04 12:23:34 -0500

    Added useState for prop title

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+
+//state
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
+    setTitle("Updated!");
     console.log("Clicked");
   }
 
@@ -13,7 +18,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
