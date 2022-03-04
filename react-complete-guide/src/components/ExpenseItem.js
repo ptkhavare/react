e125f124e08c9cf5678924401a6b2cb6b617fe8c
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-04 00:25:43 -0500

    Added dynamic data.

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022,3,23);
+  const expenseTitle = "Car Policy";
+  const expenseAmt = 254.34;
+
   //only one root component allowed in jsx
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmt}</div>
       </div>
     </div>
   );
