114cc2f1cf4210fe636c4d2637ad4007b0428662
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-06 20:29:42 -0500

    Fixed bug in Expense Form for Amount as String.

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ function ExpenseForm(props) {
     const expenseData = {
       id: Math.random().toString(),
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
